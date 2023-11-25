90f9c1143a4034fc063ec07d63eec327653b47a7
Elijah Ngocho <elijahngocho24@gmail.com>
2023-11-25 13:55:24 +0300

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+
+    for i in range(2, length):
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence[:length])
+
